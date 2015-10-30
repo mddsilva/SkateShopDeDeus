93bfac2e82eee3ed4eadd6b5f4da5d31144d8cc4
Maycon Silva <mayconddsilva@gmail.com>
2015-10-30 18:33:41 -0200

    Foi criada a classe Usuário, juntamente com seu atributos

---
+++ DeusSkateShop/src/main/java/br/univel/model/Usuario.java
@@ -0,0 +1,111 @@
+package br.univel.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import java.lang.Override;
+
+@Entity
+public class Usuario implements Serializable
+{
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   @Column(name = "id", updatable = false, nullable = false)
+   private Long id;
+   @Version
+   @Column(name = "version")
+   private int version;
+
+   @Column(nullable = false)
+   private String login;
+
+   @Column
+   private String senha;
+
+   public Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(final Long id)
+   {
+      this.id = id;
+   }
+
+   public int getVersion()
+   {
+      return this.version;
+   }
+
+   public void setVersion(final int version)
+   {
+      this.version = version;
+   }
+
+   @Override
+   public boolean equals(Object obj)
+   {
+      if (this == obj)
+      {
+         return true;
+      }
+      if (!(obj instanceof Usuario))
+      {
+         return false;
+      }
+      Usuario other = (Usuario) obj;
+      if (id != null)
+      {
+         if (!id.equals(other.id))
+         {
+            return false;
+         }
+      }
+      return true;
+   }
+
+   @Override
+   public int hashCode()
+   {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((id == null) ? 0 : id.hashCode());
+      return result;
+   }
+
+   public String getLogin()
+   {
+      return login;
+   }
+
+   public void setLogin(String login)
+   {
+      this.login = login;
+   }
+
+   public String getSenha()
+   {
+      return senha;
+   }
+
+   public void setSenha(String senha)
+   {
+      this.senha = senha;
+   }
+
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (login != null && !login.trim().isEmpty())
+         result += "login: " + login;
+      if (senha != null && !senha.trim().isEmpty())
+         result += ", senha: " + senha;
+      return result;
+   }
+}
